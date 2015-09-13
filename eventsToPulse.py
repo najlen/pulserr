2607645f6d2b50e9b770b5dcb77cd57fc7955d1c
Najlen <daniel@grandtotal.se>
2015-09-13 07:02:14 +0200

    Added file

---
+++ eventsToPulse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Consume events from rabbit queue and identify pulses. 
+Push pulses back to rabbit, in new queue.
+"""
+
+__author__ = 'Daniel Nihlen'
+__version__ = '0.1'
+__date__ = '2015-09-12'
+
